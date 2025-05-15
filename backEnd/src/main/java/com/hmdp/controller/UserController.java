db240f5ec3250cf3a0c7a557b0fa252092b8971d
ctliu <yourhierarch@gmail.com>
2025-05-15 15:10:15 +0800

    feat(user):注册功能

--- backEnd/src/main/java/com/hmdp/controller/UserController.java
+++ backEnd/src/main/java/com/hmdp/controller/UserController.java
@@ -36,9 +36,9 @@ public class UserController {
      */
     @PostMapping("code")
     public Result sendCode(@RequestParam("phone") String phone, HttpSession session) {
-        // TODO 发送短信验证码并保存验证码
-        userService.sendCode(phone,session);
-        return Result.fail("功能未完成");
+        //  发送短信验证码并保存验证码
+        Result result = userService.sendCode(phone,session);
+        return result;
     }
 
     /**
@@ -48,7 +48,8 @@ public class UserController {
     @PostMapping("/login")
     public Result login(@RequestBody LoginFormDTO loginForm, HttpSession session){
         // TODO 实现登录功能
-        return Result.fail("功能未完成");
+        Result result = userService.login(loginForm,session);
+        return result;
     }
 
     /**
--- backEnd/src/main/java/com/hmdp/service/impl/UserServiceImpl.java
+++ backEnd/src/main/java/com/hmdp/service/impl/UserServiceImpl.java
@@ -2,11 +2,13 @@ package com.hmdp.service.impl;
 
 import cn.hutool.core.util.RandomUtil;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.hmdp.dto.LoginFormDTO;
 import com.hmdp.dto.Result;
 import com.hmdp.entity.User;
 import com.hmdp.mapper.UserMapper;
 import com.hmdp.service.IUserService;
 import com.hmdp.utils.RegexUtils;
+import com.hmdp.utils.SystemConstants;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
@@ -23,6 +25,12 @@ import javax.servlet.http.HttpSession;
 @Service
 @Slf4j
 public class UserServiceImpl extends ServiceImpl<UserMapper, User> implements IUserService {
+
+    private final UserMapper userMapper;
+
+    public UserServiceImpl(UserMapper userMapper) {
+        this.userMapper = userMapper;
+    }
 
     /**
      * 用户登录
@@ -46,5 +54,45 @@ public class UserServiceImpl extends ServiceImpl<UserMapper, User> implements IU
 //        s3:发送验证码
         log.debug("发送短信验证码成功,验证码:{}",code);
         return Result.ok();
+    }
+
+    @Override
+    public Result login(LoginFormDTO loginForm, HttpSession session) {
+//        s1:校验手机号
+        String phone = loginForm.getPhone();
+        if(RegexUtils.isPhoneInvalid(phone)){
+//          不符合,返回错误信息
+
+            return Result.fail("手机号格式错误!");
+        }
+//        s2:校验验证码
+        Object cachecode = session.getAttribute("code");
+        String code =  loginForm.getCode();
+//        s2.1:不一致,报错
+        if(cachecode == null || !cachecode.equals(code)){
+            return Result.fail("验证码错误!");
+        }
+//        s2.2:一致,根据手机号查询用户
+//        User user = new User().setPhone(phone);
+//        userMapper.getUserByPhone(user);
+//        s3:数据库中用户是否存在
+        User user = query().eq("phone", phone).one();
+        if(user == null){
+//        s3.1:不存在,创建用户,写入数据库
+            user = createUserWithPhone(phone);
+        }
+//        s3.2:存在.
+
+//        保存用户信息到 session
+        session.setAttribute("user",user);
+        return Result.ok();//基于 Session 登录,无需返回任何东西
+    }
+
+    private User createUserWithPhone(String phone) {
+        User user = new User();
+        user.setPhone(phone);
+        user.setNickName(SystemConstants.USER_NICK_NAME_PREFIX + RandomUtil.randomString(10));
+        save(user);
+        return user;
     }
 }
