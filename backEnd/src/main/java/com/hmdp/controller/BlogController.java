1f48368dc26c65db9b9096bc0432821e80482251
ctliu <yourhierarch@gmail.com>
2025-06-14 10:03:16 +0800

    feat(blog)新增查询笔记功能

--- backEnd/src/main/java/com/hmdp/controller/BlogController.java
+++ backEnd/src/main/java/com/hmdp/controller/BlogController.java
@@ -65,19 +65,29 @@ public class BlogController {
 
     @GetMapping("/hot")
     public Result queryHotBlog(@RequestParam(value = "current", defaultValue = "1") Integer current) {
-        // 根据用户查询
-        Page<Blog> page = blogService.query()
-                .orderByDesc("liked")
-                .page(new Page<>(current, SystemConstants.MAX_PAGE_SIZE));
-        // 获取当前页数据
-        List<Blog> records = page.getRecords();
-        // 查询用户
-        records.forEach(blog ->{
-            Long userId = blog.getUserId();
-            User user = userService.getById(userId);
-            blog.setName(user.getNickName());
-            blog.setIcon(user.getIcon());
-        });
-        return Result.ok(records);
+//        // 根据用户查询
+//        Page<Blog> page = blogService.query()
+//                .orderByDesc("liked")
+//                .page(new Page<>(current, SystemConstants.MAX_PAGE_SIZE));
+//        // 获取当前页数据
+//        List<Blog> records = page.getRecords();
+//        // 查询用户
+//        records.forEach(blog ->{
+//            Long userId = blog.getUserId();
+//            User user = userService.getById(userId);
+//            blog.setName(user.getNickName());
+//            blog.setIcon(user.getIcon());
+//        });
+        return blogService.queryHotBlog(current);
+    }
+
+    /**
+     * 查询 blog
+     * @param id
+     * @return
+     */
+    @GetMapping("/{id}")
+    public Result queryBlog(@PathVariable(value = "id") Long id){
+        return blogService.queryBlogById(id);
     }
 }
--- backEnd/src/main/java/com/hmdp/service/impl/BlogServiceImpl.java
+++ backEnd/src/main/java/com/hmdp/service/impl/BlogServiceImpl.java
@@ -1,11 +1,20 @@
 package com.hmdp.service.impl;
 
+import com.hmdp.dto.Result;
 import com.hmdp.entity.Blog;
 import com.hmdp.mapper.BlogMapper;
 import com.hmdp.service.IBlogService;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.hmdp.service.IUserService;
 import org.springframework.stereotype.Service;
 
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.hmdp.entity.User;
+import com.hmdp.utils.SystemConstants;
+import org.springframework.web.bind.annotation.*;
+
+import javax.annotation.Resource;
+import java.util.List;
 /**
  * <p>
  *  服务实现类
@@ -16,5 +25,40 @@ import org.springframework.stereotype.Service;
  */
 @Service
 public class BlogServiceImpl extends ServiceImpl<BlogMapper, Blog> implements IBlogService {
+    @Resource
+    private IUserService userService;
+    @Resource
+    private IBlogService blogService;
 
+    @Override
+    public Result queryBlogById(Long id) {
+        //1查询 blog
+        Blog blog = getById(id);
+        if(blog == null) {
+            return Result.fail("笔记不存在");
+        }
+//        查询 blog的用户
+        queryById(blog);
+        return Result.ok(blog);
+    }
+
+    private void queryById(Blog blog) {
+        Long userId = blog.getUserId();
+        User user = userService.getById(userId);
+        blog.setName(user.getNickName());
+        blog.setIcon(user.getIcon());
+    }
+
+    @Override
+    public Result queryHotBlog(Integer current) {
+        // 根据用户查询
+        Page<Blog> page = blogService.query()
+                .orderByDesc("liked")
+                .page(new Page<>(current, SystemConstants.MAX_PAGE_SIZE));
+        // 获取当前页数据
+        List<Blog> records = page.getRecords();
+        // 查询用户
+        records.forEach(this::queryById);
+        return Result.ok(records);
+    }
 }
