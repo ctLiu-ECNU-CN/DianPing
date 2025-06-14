fbd14a8c52033ee44bb493d4b1423e30cafba6c7
ctliu <yourhierarch@gmail.com>
2025-06-14 17:37:08 +0800

    feat(user) 实现查看用户发布的笔记功能

--- backEnd/src/main/java/com/hmdp/controller/BlogController.java
+++ backEnd/src/main/java/com/hmdp/controller/BlogController.java
@@ -109,4 +109,22 @@ public class BlogController {
     public Result queryBlogLikes(@PathVariable(value = "id") Long id){
         return blogService.queryTopLikes(id);
     }
+
+    /**
+     * 根据用户 id查询笔记
+     * @param current
+     * @param id
+     * @return
+     */
+    @GetMapping("/of/user")
+    public Result queryBlogByUserId(
+            @RequestParam(value = "current", defaultValue = "1") Integer current,
+            @RequestParam("id") Long id) {
+        // 根据用户查询
+        Page<Blog> page = blogService.query()
+                .eq("user_id", id).page(new Page<>(current, SystemConstants.MAX_PAGE_SIZE));
+        // 获取当前页数据
+        List<Blog> records = page.getRecords();
+        return Result.ok(records);
+    }
 }
